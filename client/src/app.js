80bab4af1bd3ce4f93d1d6400dd79aa87ed3daf5
akashsingh-github <87904483+akashsingh-github@users.noreply.github.com>
2021-08-14 18:14:44 +0530

    client src creation

---
+++ client/src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { ComponentToPrint } from './ComponentToPrint';
+// import Print from "./Component/print";
+// import Form from "./Component/Form/index";
+import OrderList from "./Component/OrderList/index";
+
+function App(){
+  return(
+    <>
+      {/* <Print/> */}
+      <OrderList/>
+      {/* <Form/> */}
+    </>
+  )
+}
+
+export default App;
